2b949fd6ae18610df29a95e0fc8e3491077a2473
Zhang, Feng <jumphone@163.com>
2018-07-24 14:11:40 -0400

    Update doForAll.py

--- utilities/doForAll.py
+++ utilities/doForAll.py
@@ -8,20 +8,20 @@ def Work(sprint_out_dir,a):
     subprocess.Popen('python getA2I.py 0 '+sprint_out_dir+' '+sprint_out_dir+'/A_to_I.res',shell=True).wait()
     if os.path.exists(sprint_out_dir+'/all_combined.zz.sorted.bam')==False:
 
-            subprocess.Popen('python zz2sam.py '+sprint_out_dir+'/all_combined.zz.sorted.gz ',shell=True).wait()
-            subprocess.Popen('cat SAMheader.txt '+sprint_out_dir+'/all_combined.zz.sorted.gz.sam > '+sprint_out_dir+'/all_combined.zz.sorted.gz.sam.header',shell=True).wait()
-            subprocess.Popen('samtools view -bS '+sprint_out_dir+'/all_combined.zz.sorted.gz.sam.header > '+sprint_out_dir+'/all_combined.zz.bam',shell=True).wait()
+            subprocess.Popen('python zz2sam.py '+sprint_out_dir+'/all_combined.zz.sorted ',shell=True).wait()
+            subprocess.Popen('cat SAMheader.txt '+sprint_out_dir+'/all_combined.zz.sorted.sam > '+sprint_out_dir+'/all_combined.zz.sorted.sam.header',shell=True).wait()
+            subprocess.Popen('samtools view -bS '+sprint_out_dir+'/all_combined.zz.sorted.sam.header > '+sprint_out_dir+'/all_combined.zz.bam',shell=True).wait()
             subprocess.Popen('samtools sort '+sprint_out_dir+'/all_combined.zz.bam -f '+sprint_out_dir+'/all_combined.zz.sorted.bam',shell=True ).wait()
 
-            subprocess.Popen('rm -rf '+sprint_out_dir+'/all_combined.zz.sorted.gz.sam',shell=True).wait()
-            subprocess.Popen('rm -rf '+sprint_out_dir+'/all_combined.zz.sorted.gz.sam.header',shell=True).wait()
+            subprocess.Popen('rm -rf '+sprint_out_dir+'/all_combined.zz.sorted.sam',shell=True).wait()
+            subprocess.Popen('rm -rf '+sprint_out_dir+'/all_combined.zz.sorted.sam.header',shell=True).wait()
             subprocess.Popen('rm -rf '+sprint_out_dir+'/all_combined.zz.bam',shell=True).wait()
             subprocess.Popen('samtools stats '+sprint_out_dir+'/all_combined.zz.sorted.bam > '+sprint_out_dir+'/all_combined.zz.sorted.bam.stats',shell=True).wait()
     
     subprocess.Popen('samtools depth -b '+ALL_A_to_I+' '+sprint_out_dir+'/all_combined.zz.sorted.bam'+' > '+sprint_out_dir+'/ALL_A_to_I.depth ',shell=True).wait()
-    subprocess.Popen('python  getAlt.py '+sprint_out_dir+'/all_combined.zz.sorted.gz  '+ALL_A_to_I+' '+sprint_out_dir+'/ALL_A_to_I.alt ',shell=True).wait()
+    subprocess.Popen('python  getAlt.py '+sprint_out_dir+'/all_combined.zz.sorted  '+ALL_A_to_I+' '+sprint_out_dir+'/ALL_A_to_I.alt ',shell=True).wait()
     subprocess.Popen('python combineAltDep.py '+sprint_out_dir, shell=True).wait()
-    subprocess.Popen('python statGENE.py '+sprint_out_dir, shell=True).wait()
+    #subprocess.Popen('python statGENE.py '+sprint_out_dir, shell=True).wait()
     
   
 fa=open('dead_metadata.cart.2017-02-13T07-29-02.705473.json.txt_new.paired')
